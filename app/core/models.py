855b91bd7590a0c3def88079db969831e4d7d81f
Jon Connolly <jontconnolly@gmail.com>
2019-03-14 13:29:31 -0700

    add recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -61,3 +61,22 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # using string 'Ingredient' rather than class Ingredient
+    # doesn't require class to be declared first
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
