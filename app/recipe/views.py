605a28030de1095def299c2d5c910afd90906623
Jon Connolly <jontconnolly@gmail.com>
2019-03-14 11:32:57 -0700

    refactore tag view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,14 +7,13 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    '''manage tags in the database'''
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    '''base viewset'''
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         '''return objects for the current auth user only'''
@@ -26,21 +25,13 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeAttrViewSet):
+    '''manage tags in the database'''
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
     '''manage ingredients in teh database'''
-
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
-
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        '''return objects for current auth user'''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        '''create a new ingredient'''
-        serializer.save(user=self.request.user)
