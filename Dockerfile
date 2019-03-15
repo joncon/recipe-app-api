be6875532898563404b5a174bcc5ba95db46c7fc
Jon Connolly <jontconnolly@gmail.com>
2019-03-15 11:07:51 -0700

    added image field to recipe model

--- Dockerfile
+++ Dockerfile
@@ -5,17 +5,22 @@ ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
 
-RUN apk add --update --no-cache postgresql-libs
+RUN apk add --update --no-cache postgresql-libs jpeg-dev
 RUN apk add --update --no-cache --virtual .tmp-build-deps \
-    gcc libc-dev linux-headers postgresql-dev
+    gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
 RUN pip install -r /requirements.txt
 
-# RUN apk del .tmp-build-deps
+RUN apk del .tmp-build-deps
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
 # -D creates user for application process only no home dir
+#media is for uploads
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
 RUN adduser -D user
+RUN chown -R user:user /vol/
+RUN chmod -R 775 /vol/web
 #Switch to user
 USER user
--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,17 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 # for referencing user model
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, file_name):
+    '''generate file path to new recipe image'''
+    ext = file_name.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -77,6 +86,7 @@ class Recipe(models.Model):
     # doesn't require class to be declared first
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
